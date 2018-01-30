a73ccff668d84fb1f280509c9f92443970cd868e
jamesalbert <jimmychanga.cheese@gmail.com>
2018-01-29 19:06:15 -0800

    added more functions

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Grafana for Nim
 Grafana module for nim 🤷‍♂️
 
 
-*note*: not stable enough to release to nimble
-
 ## Installing
 
 `nimble install https://github.com/jamesalbert/grafana-nim.git@#head`
@@ -21,9 +19,22 @@ It's not really a "testing environment", just a container to mess around with ap
 
 ## Examples
 
+Credentials are defaults for InfluxDB and Grafana
+
 ```nim
 import grafana
 
-let gc = newGrafanaClient("localhost:3000", "user", "pass") # or ("localhost:3000", "api-key")
-gc.Dashboards()
+let gc = newGrafanaClient("grafana:3000", "admin", "admin") # or ("grafana:3000", "api-key")
+echo gc.NewDashboard( %* {
+  "title": "new dash"
+})
+echo gc.NewInfluxDBDatasource( %* {
+  "host": "localhost",
+  "name": "BTCUSD",
+  "database": "btc_usd",
+  "user": "root",
+  "pass": "root"
+})
+echo gc.Datasources()
+echo gc.Dashboards()
 ```
--- grafana.nim
+++ grafana.nim
@@ -8,7 +8,7 @@ type
 
 
 proc newGrafanaClient* (host: string, user: string, pass: string): GrafanaClient =
-  let url = "http://" & host & "/"
+  let url = "http://" & host & "/api/"
   let client = newHttpClient()
   client.headers = newHttpHeaders({
     "Content-Type": "application/json",
@@ -19,7 +19,7 @@ proc newGrafanaClient* (host: string, user: string, pass: string): GrafanaClient
 
 
 proc newGrafanaClient* (host: string, key: string): GrafanaClient =
-  let url = "http://" & host & "/"
+  let url = "http://" & host & "/api/"
   let client = newHttpClient()
   client.headers = newHttpHeaders({
     "Content-Type": "application/json",
@@ -41,7 +41,7 @@ method Post (self: GrafanaClient, route: string, body: JsonNode): JsonNode {.bas
 
 method Login (self: GrafanaClient): JsonNode {.base.} =
   try:
-    return self.Request("api/login/ping")
+    return self.Request("login/ping")
   except HttpRequestError as e:
     return %* {
       "error": e.msg
@@ -55,7 +55,7 @@ Get Methods
 
 method AdminSettings* (self: GrafanaClient): JsonNode {.base.} =
   try:
-    return self.Request("api/admin/settings")
+    return self.Request("admin/settings")
   except HttpRequestError as e:
     return %* {
       "error": e.msg
@@ -64,7 +64,7 @@ method AdminSettings* (self: GrafanaClient): JsonNode {.base.} =
 
 method Settings* (self: GrafanaClient): JsonNode {.base.} =
   try:
-    return self.Request("api/frontend/settings")
+    return self.Request("frontend/settings")
   except HttpRequestError as e:
     return %* {
       "error": e.msg
@@ -73,7 +73,7 @@ method Settings* (self: GrafanaClient): JsonNode {.base.} =
 
 method Keys* (self: GrafanaClient): JsonNode {.base.} =
   try:
-    return self.Request("api/auth/keys")
+    return self.Request("auth/keys")
   except HttpRequestError as e:
     return %* {
       "error": e.msg
@@ -82,7 +82,7 @@ method Keys* (self: GrafanaClient): JsonNode {.base.} =
 
 method Orgs* (self: GrafanaClient): JsonNode {.base.} =
   try:
-    return self.Request("api/orgs")
+    return self.Request("orgs")
   except HttpRequestError as e:
     return %* {
       "error": e.msg
@@ -91,7 +91,16 @@ method Orgs* (self: GrafanaClient): JsonNode {.base.} =
 
 method Users* (self: GrafanaClient): JsonNode {.base.} =
   try:
-    return self.Request("api/users")
+    return self.Request("users")
+  except HttpRequestError as e:
+    return %* {
+      "error": e.msg
+    }
+
+
+method Dashboard* (self: GrafanaClient, dashboard: string): JsonNode {.base.} =
+  try:
+    return self.Request("dashboards/db/" & dashboard)
   except HttpRequestError as e:
     return %* {
       "error": e.msg
@@ -100,7 +109,65 @@ method Users* (self: GrafanaClient): JsonNode {.base.} =
 
 method Dashboards* (self: GrafanaClient): JsonNode {.base.} =
   try:
-    return self.Request("api/dashboards/db")
+    return self.Request("dashboards/home")
+  except HttpRequestError as e:
+    return %* {
+      "error": e.msg
+    }
+
+
+method DashboardTags* (self: GrafanaClient): JsonNode {.base.} =
+  try:
+    return self.Request("dashboards/tags")
+  except HttpRequestError as e:
+    return %* {
+      "error": e.msg
+    }
+
+
+method SearchDashboards* (self: GrafanaClient, query: string, tag: string = "", starred = false, tagcloud = false): JsonNode {.base.} =
+  try:
+    var query = "?query=" & query
+    query.add("&tag=" & tag)
+    query.add("&starred=" & $starred)
+    query.add("&tagcloud=" & $tagcloud)
+    return self.Request("search" & query)
+  except HttpRequestError as e:
+    return %* {
+      "error": e.msg
+    }
+
+
+method Datasources* (self: GrafanaClient): JsonNode {.base.} =
+  try:
+    return self.Request("datasources")
+  except HttpRequestError as e:
+    return %* {
+      "error": e.msg
+    }
+
+
+method Datasource* (self: GrafanaClient, id: int): JsonNode {.base.} =
+  try:
+    return self.Request("datasources/" & $id)
+  except HttpRequestError as e:
+    return %* {
+      "error": e.msg
+    }
+
+
+method Datasource* (self: GrafanaClient, name: string): JsonNode {.base.} =
+  try:
+    return self.Request("datasources/name/" & name)
+  except HttpRequestError as e:
+    return %* {
+      "error": e.msg
+    }
+
+
+method DatasourceId* (self: GrafanaClient, name: string): JsonNode {.base.} =
+  try:
+    return self.Request("datasources/id/" & name)
   except HttpRequestError as e:
     return %* {
       "error": e.msg
@@ -111,19 +178,37 @@ discard """
 Post Methods
 """
 
-method NewDashboard* (self: GrafanaClient, title: string, tags: seq[string] = @[]): JsonNode {.base.} =
+method NewDashboard* (self: GrafanaClient, opts: JsonNode): JsonNode {.base.} =
   try:
-    return self.Post("api/dashboards/db", %* {
+    return self.Post("dashboards/db", %* {
       "dashboard": {
         "id": nil,
-        "title": title,
-        "tags": tags,
+        "title": opts["title"].getStr,
+        "tags": if opts.hasKey("tags"): opts["tags"].elems else: @[],
         "timezone": "browser",
         "rows": [ {} ],
         "schemaVersion": 6,
         "version": 0
       },
       "overwrite": true
+    })
+  except HttpRequestError as e:
+    return %* {
+      "error": e.msg
+    }
+
+
+method NewInfluxDBDatasource* (self: GrafanaClient, opts: JsonNode): JsonNode {.base.} =
+  try:
+    return self.Post("datasources", %* {
+      "name": opts["name"].getStr,
+      "database": opts["database"].getStr,
+      "user": opts["user"].getStr,
+      "password": opts["pass"].getStr,
+      "url": "http://" & opts["host"].getStr & ":8086",
+      "type": "influxdb",
+      "access": "direct",
+      "isDefault": false
     })
   except HttpRequestError as e:
     return %* {
